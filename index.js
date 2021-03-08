1d0f8b30569b3f68d05f2f099e1ee8b757989dd7
milrosen <61394098+milrosen@users.noreply.github.com>
2021-03-07 21:21:33 -0700

    mongodb connection sucessfull

--- index.js
+++ index.js
@@ -2,34 +2,51 @@ const dontenv = require('dotenv');
 dontenv.config();
 const fs = require('fs');
 
+const { MongoClient } = require('mongodb');
+const url = `mongodb+srv://milrosen:${process.env.MONGOPASS}@cluster0.vrxm7.mongodb.net/admin?retryWrites=true&w=majority`;
+const mongoClient = new MongoClient(url);
+
+async function run() {
+    try {
+        await mongoClient.connect();
+        console.log("Connected correctly to server");
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await mongoClient.close();
+    }
+}
+run().catch(console.dir);
+
 const Discord = require('discord.js');
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const disClient = new Discord.Client();
+disClient.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 commandFiles.forEach(file => {
 	const command = require(`./commands/${file}`)
 
-	client.commands.set(command.name, command);
+	disClient.commands.set(command.name, command);
 })
 
-client.once('ready', () => {
+disClient.once('ready', () => {
 	console.log('ready');
 });
 
-client.login(process.env.TOKEN);
+disClient.login(process.env.TOKEN);
 
-client.on('message', message => {
+disClient.on('message', message => {
 	const content = message.content;
 	if (!content.startsWith('!') || message.author.bot) return;
 
 	const args = content.slice(1).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) return;
+	if (!disClient.commands.has(command)) return;
 	try {
-		client.commands.get(command).execute(message, args);
+		disClient.commands.get(command).execute(message, args);
 	} catch (error) {
 		console.error(error);
 		message.reply('if there were competent coders working on me, that would\'ve done something');
